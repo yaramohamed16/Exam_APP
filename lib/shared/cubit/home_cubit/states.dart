d238393d49d2f467ac5efd0446a3db9f6ac73bd1
Yara <yarayassin1611@gmail.com>
2024-03-16 02:15:14 +0200

    Last Commit

--- lib/shared/cubit/home_cubit/states.dart
+++ lib/shared/cubit/home_cubit/states.dart
@@ -7,3 +7,25 @@ class ExamPackagesLoading extends ExamsStats {}
 class ExamPackagesSuccess extends ExamsStats {}
 
 class ExamPackagesError extends ExamsStats {}
+
+class ExamAnswersLoading extends ExamsStats {}
+
+class ExamAnswersSuccess extends ExamsStats {}
+
+class ExamAnswersError extends ExamsStats {}
+
+class HomeAdsLoading extends ExamsStats {}
+
+class HomeAdsSuccess extends ExamsStats {}
+
+class HomeAdsError extends ExamsStats {}
+
+class UpdateIndex extends ExamsStats {}
+
+
+
+class ExamsState extends ExamsStats {
+  final String? opt;
+
+  ExamsState({this.opt});
+}
