22dda913be69dbc72258c57d62a5549255b414ce
Yara <yarayassin1611@gmail.com>
2024-03-18 01:34:12 +0200

    Last Commit

--- lib/modules/login/widgets/text_form_field.dart
+++ lib/modules/login/widgets/text_form_field.dart
@@ -7,14 +7,17 @@ Widget defaultTextFormField({
   required TextInputType type,
   bool isPassword = false,
   IconData? suffix,
-  void Function(String)? onSubmit, // Update the type to accept a string
-  void Function(String)? onChange, // Update the type to accept a string
-
+  void Function(String)? onSubmit,
+  void Function(String)? onChange,
   VoidCallback? suffixPressed,
 }) =>
     TextFormField(
       controller: control,
       decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: mainColor),
+        ),
         hintText: text,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12.0),
@@ -22,24 +25,15 @@ Widget defaultTextFormField({
             color: Color(0xffD1D5DB),
           ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: Color(0xffD1D5DB),
-          ),
-        ),
         suffixIcon: suffix != null
             ? IconButton(
-          onPressed: suffixPressed,
-          icon: Icon(suffix,color: mainColor),
-        )
+                onPressed: suffixPressed,
+                icon: Icon(suffix, color: mainColor),
+              )
             : null,
       ),
       keyboardType: type,
       obscureText: isPassword,
       onFieldSubmitted: onSubmit,
-      // Pass onSubmit directly
       onChanged: onChange,
-      // Pass onChange directly
-
     );
